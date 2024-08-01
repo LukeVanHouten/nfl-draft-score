87595d4a41edc2a8c7ebc7a918000c3d2611c338
LukeVanHouten <lukevh@uw.edu>
2024-08-01 13:35:23 -0700

    functionalized draft score

--- draft.R
+++ draft.R
@@ -1,99 +1,71 @@
 library(tidyverse)
 
 draft_df <- read.csv("2022-2024-draft.csv")
-# draft <- read.csv("draft.csv")
 
-rounds <- 1:7
-years <- 2024
-log_bias <- 1
+log_bias <- exp(1) - 1
 
-# draft_df <- draft %>%
-#     filter(Round %in% rounds) %>%
-#     select(-Round)
+draft_scores <- function(years, num_teams, transformation) {
+    draft_picks <- draft_df %>%
+        select(paste0("X", as.character(years))) %>%
+        mutate(pick = rownames(.)) %>%
+        pivot_longer(cols = colnames(select(., -pick)), 
+                     names_to = "year") %>%
+        group_by(value, year) %>%
+        summarize(pick = list(pick)) %>%
+        pivot_wider(names_from = year, values_from = pick) %>%
+        filter(value != "") %>%
+        rowwise() %>%
+        mutate(picks = list(c_across(everything()))) %>%
+        ungroup() %>%
+        select(value, picks)
+    
+    pivot_df <- t(draft_picks$picks)
+    colnames(pivot_df) <- draft_picks$value
+    
+    if (transformation == "reciprocal") {
+        tukey <- function(x) {
+            sum(1 / as.numeric(unlist(x)))
+        }
+    } else if (transformation == "root") {
+        tukey <- function(x) {
+            sum(1 / sqrt(as.numeric(unlist(x))))
+        }
+    } else if (transformation == "log") {
+        tukey <- function(x) {
+            sum(1 / log(as.numeric(unlist(x)) + log_bias))
+        }
+    } else {
+        return("Transformations must be either 'reciprocal', 'root', or 'log'")
+    }
+    
+    scores_df <- as.data.frame(sort(apply(pivot_df, 2, tukey), 
+                                    decreasing = TRUE))
+    colnames(scores_df) <- "score"
+    
+    if (is.numeric(num_teams)) {
+        scores_df <- head(scores_df, num_teams)
+        top_teams <- paste("Top", num_teams)
+    } else if (num_teams == "all") {
+        top_teams <- ""
+    } else {
+        return(gsub("\\\n", "", "The number of teams must either be numeric or
+ 'all' for all teams"))
+    }
+    
+    p <- ggplot(data = scores_df, 
+           aes(x = reorder(rownames(scores_df), -score), y = score)) +
+        geom_col(fill = "purple", color = "gold") +
+        geom_text(aes(label = round(score, 4)), 
+                  position = position_dodge(width = 0.9), 
+                  angle = 45, hjust = -0.1, vjust = -0.3) +
+        labs(title = paste(top_teams, str_to_title(transformation), 
+                           "Transformed College Draft Scores in the",
+                           paste(years, collapse = ", "), "NFL Draft"), 
+             x = "School", y = "Score") +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1), 
+              legend.position = "none")
+    return(p)
+}
 
-draft_df <- draft_df %>%
-    select(paste0("X", as.character(years)))
-
-draft_picks <- draft_df %>%
-    mutate(pick = rownames(draft_df))
-
-combined_df <- draft_picks %>%
-    pivot_longer(cols = colnames(select(draft_picks, -pick)), 
-                 names_to = "year") %>%
-    group_by(value, year) %>%
-    summarize(pick = list(pick)) %>%
-    pivot_wider(names_from = year, values_from = pick) %>%
-    filter(value != "") %>%
-    rowwise() %>%
-    mutate(picks = list(c_across(everything()))) %>%
-    ungroup() %>%
-    select(value, picks)
-
-pivot_df <- t(combined_df$picks)
-colnames(pivot_df) <- combined_df$value
-
-draft_scores <- as.data.frame(sort(apply(pivot_df, 2, function(x){
-    sum(1 / as.numeric(unlist(x)))
-}), decreasing = TRUE))
-
-draft_scores_sqrt <- as.data.frame(sort(apply(pivot_df, 2, function(x){
-    sum(1 / sqrt(as.numeric(unlist(x))))
-}), decreasing = TRUE))
-
-draft_scores_log <- as.data.frame(sort(apply(pivot_df, 2, function(x){
-    sum(1 / log(as.numeric(unlist(x)) + log_bias))
-}), decreasing = TRUE))
-
-colnames(draft_scores)[1] <- "score"
-colnames(draft_scores_sqrt)[1] <- "score"
-colnames(draft_scores_log)[1] <- "score"
-
-# draft_scores$score <- draft_scores$score / max(draft_scores$score)
-# draft_scores_sqrt$score <- draft_scores_sqrt$score /
-#     max(draft_scores_sqrt$score)
-# draft_scores_log$score <- draft_scores_log$score / max(draft_scores_log$score)
-
-p <- ggplot(data = draft_scores, 
-            aes(x = reorder(rownames(draft_scores), -score), y = score)) +
-    geom_col(fill = "purple", color = "gold") +
-    geom_text(aes(label = round(score, 4)), 
-              position = position_dodge(width = 0.9), 
-              angle = 45, hjust = -0.1, vjust = -0.3) +
-    labs(title = paste("College Draft Scores in Round", 
-                       paste(rounds, collapse = ", "), "of the", 
-                       paste(years, collapse = ", "), "NFL Draft"), 
-         x = "School", y = "Score") +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1), 
-          legend.position = "none")
-
-p_sqrt <- ggplot(data = draft_scores_sqrt, 
-                 aes(x = reorder(rownames(draft_scores_sqrt), -score),  
-                     y = score)) +
-    geom_col(fill = "purple", color = "gold") +
-    geom_text(aes(label = round(score, 4)), 
-              position = position_dodge(width = 0.9), 
-              angle = 45, hjust = -0.1, vjust = -0.3) +
-    labs(title = paste("Square Root Transformed College Draft Scores in Round", 
-                       paste(rounds, collapse = ", "), "of the", 
-                       paste(years, collapse = ", "), "NFL Draft"),  
-         x = "School", y = "Score") +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1), 
-          legend.position = "none")
-
-p_log <- ggplot(data = draft_scores_log, 
-                aes(x = reorder(rownames(draft_scores_log), -score),
-                    y = score)) +
-    geom_col(fill = "purple", color = "gold") +
-    geom_text(aes(label = round(score, 4)), 
-              position = position_dodge(width = 0.9), 
-              angle = 45, hjust = -0.1, vjust = -0.3) +
-    labs(title = paste("Log Transformed College Draft Scores in Round", 
-                       paste(rounds, collapse = ", "), "of the", 
-                       paste(years, collapse = ", "), "NFL Draft"), 
-         x = "School", y = "Score") +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1), 
-          legend.position = "none")
-
-p
-p_sqrt
-p_log
+scores <- draft_scores(2024, "all", "log")
+scores

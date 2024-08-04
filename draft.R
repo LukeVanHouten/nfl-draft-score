71b626efb11fae4c6d64068f36429c5e3e2f1f77
LukeVanHouten <lukevh@uw.edu>
2024-08-03 22:49:34 -0700

    per school function (slow af)

--- draft.R
+++ draft.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+options(dplyr.summarise.inform = FALSE)
 
 draft_df <- read.csv("drafts.csv")
 
@@ -69,8 +70,11 @@ draft_scores <- function(years, top_teams, transformation) {
         draft <- "NFL Drafts"
     }
     
-    p <- ggplot(data = scores_df, 
-                aes(x = reorder(rownames(scores_df), -score), y = score)) +
+    scores_df <- cbind(rownames(scores_df), scores_df) %>%
+        `colnames<-`(c("school", "score")) %>%
+        `rownames<-`(1:nrow(scores_df))
+    
+    p <- ggplot(data = scores_df, aes(x = reorder(school, -score), y = score)) +
         geom_col(fill = "purple", color = "gold") +
         geom_text(aes(label = round(score, 4)), color = "gold",
                   position = position_dodge(width = 0.9), angle = 90, 
@@ -83,8 +87,59 @@ draft_scores <- function(years, top_teams, transformation) {
     return(p)
 }
 
-scores <- draft_scores(years = "all", top_teams = 50, 
+scores <- draft_scores(years = "all", top_teams = "all", 
                        transformation = "log")
 scores
 
-scores_df <- scores$plot_env$scores_df
+draft_scores_df <- scores$plot_env$scores_df
+
+get_schools <- function(school, years, transformation) {
+    if (all(years %in% 1936:2024)) {
+        years <- years
+    } else if (years == "all") {
+        years <- 1936:2024
+    } else {
+        return(gsub("\\\n", "", "Years must be within the 
+ years 1936-2024, or 'all' for all of these years"))
+    }
+    
+    if (!(school %in% draft_scores_df$school)) {
+        return("School must be a valid team")
+    }
+
+    year_scores <- as.data.frame(t(sapply(years, function(x) {
+        draft_scores(years = x, top_teams = "all", 
+                     transformation = transformation)$plot_env$scores_df})))
+    indices <- lapply(year_scores$school, function(y) {
+        idx <- which(y == school)
+        if (length(idx) == 0) NA else idx
+    })
+    school_scores <- unlist(Map(function(s, i) ifelse(is.na(i), 0, s[i]), 
+                                year_scores$score, indices))
+    schools_df <- as.data.frame(cbind(years, school_scores))
+
+    if (length(years) == 1) {
+        year_range <- years
+        draft <- "NFL Draft"
+    } else {
+        year_range <- paste0(min(years), "-", max(years))
+        draft <- "NFL Drafts"
+    }
+    
+    school_plot <- ggplot(data = schools_df, aes(x = years,
+                                                 y = school_scores)) +
+        geom_col(fill = "purple", color = "gold") +
+        geom_text(aes(label = round(school_scores, 4)), color = "gold",
+                  position = position_dodge(width = 0.9), angle = 90,
+                  hjust = 1.2, vjust = 0.5) +
+        scale_x_continuous(breaks = years) + 
+        labs(title = paste0(school, "'s ", str_to_title(transformation), 
+                            "-Transformed College Draft Scores in the ", 
+                            year_range, " ", draft), x = "Years", y = "Score") +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1),
+              legend.position = "none")
+    return(school_plot)
+}
+
+schools <- get_schools("Washington", "all", "log")
+schools
